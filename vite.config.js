e0287e4627f9f33c2ae6a622d86d3e2bbfd4f0a3
Rijul Ugawekar <rnugawek@syr.edu>
2024-08-27 18:04:39 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/react-portfolio/',  // Ensure this matches your repository name
-});
+export default {
+  base: '/react-portfolio/',
+}
