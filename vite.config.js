5276bbe0f44ef9edd2e17ecb73d779656a986690
techwthjul <rnugawek@syr.edu>
2024-08-30 23:18:38 -0400

    Resolved merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-<<<<<<< HEAD
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-=======
-export default defineConfig({
-  plugins: [react()],
-  base: '/react-portfolio/',
->>>>>>> e7d31b802b472bb3ece2637001df8c64dc473ed1
+  base: '/react-portfolio/', // This line is added to set the base path
   // other configurations
 });
